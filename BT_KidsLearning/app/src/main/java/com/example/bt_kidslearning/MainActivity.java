e706495c3e5f22fdf27ce3af592e9ae8be726c84
Như Khánh <sanakuti26@gmail.com>
2025-03-20 21:00:19 +0700

    them ma de sang mh

--- BT_KidsLearning/app/src/main/java/com/example/bt_kidslearning/MainActivity.java
+++ BT_KidsLearning/app/src/main/java/com/example/bt_kidslearning/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.bt_kidslearning;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        Button home = findViewById(R.id.btnHome);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mhhome = new Intent(MainActivity.this, MH2Activity.class);
+                startActivity(mhhome);
+            }
         });
     }
 }
